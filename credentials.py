12928738524d3fb95e13b5a75fa183487f9cffeb
Otybrian <otieno.brian@student.moringaschool.com>
2022-02-20 02:59:31 +0300

    add a method that saves user credentials entered

--- credentials.py
+++ credentials.py
@@ -1,3 +1,5 @@
+import random
+import string
 from user import User
 
 class Credentials:
@@ -16,4 +18,22 @@ class Credentials:
         The method confirm_user determines if the user details entered is in the user_list
         """
 
-        
+        def __init__(self, user_account, user_name, password):
+            """
+            This method defines the credentials that have been entered by the user to be stored
+            """
+
+            self.user_account = user_account
+            self.user_name = user_name
+            self.password = password
+
+        def save_credentials(self):
+            Credentials.user_credential_list.append(self)
+            """
+            This method is to store a new set of credentials entered by a user by adding them to the 
+            user_credential_list
+            """
+
+        @classmethod 
+            
+
