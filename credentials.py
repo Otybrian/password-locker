90bd7a0c4f49f1d82160e8314474bc2d9a906de3
Otybrian <otieno.brian@student.moringaschool.com>
2022-02-20 01:37:47 +0300

    transfer class user and methods to user.py file

--- credentials.py
+++ credentials.py
@@ -1,38 +1,4 @@
-class User:
-    """
-    Creating a class and calling it User. The class will be generating new instances of a user.
-    """
-    user_list = []
-    """
-    An empty list called user_list will store the user details
-    """
-    def _init_(self, username, user_password):
-        self.username = username
-        self.user_password = user_password
-        """
-        This method defines a user's properties
-        """
 
-    def _init_(self):
-        User.user_list.append(self)
-
-        """
-        The method that will save a new user by adding their details to the user_list
-        """
-
-    @classmethod
-    def show_user_details(cls):
-        return cls.user_list
-
-
-    def remove_user_details(self):
-
-        User.user_list.remove(self)
-
-        """
-        A classmethod decorator to declare class methods to display and 
-        remove user details
-        """
 
     class Credentials:
 
@@ -46,3 +12,5 @@ class User:
                 if(user.username == username and user.user_password == user_password):
                     saved_user == user.username
             return saved_user
+        """
+        The method confirm_user determines if the user details entered is in the user_list"""
---
+++ user.py
@@ -0,0 +1,35 @@
+class User:
+    """
+    Creating a class and calling it User. The class will be generating new instances of a user.
+    """
+    user_list = []
+    """
+    An empty list called user_list will store the user details
+    """
+    def _init_(self, username, user_password):
+        self.username = username
+        self.user_password = user_password
+        """
+        This method defines a user's properties
+        """
+
+    def _init_(self):
+        User.user_list.append(self)
+
+        """
+        The method that will save a new user by adding their details to the user_list
+        """
+
+    @classmethod
+    def show_user_details(cls):
+        return cls.user_list
+
+
+    def remove_user_details(self):
+
+        User.user_list.remove(self)
+
+        """
+        A classmethod decorator to declare class methods to display and 
+        remove user details
+        """
