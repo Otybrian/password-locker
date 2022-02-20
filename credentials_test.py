47fdf12d2c9ee7fcb70ee6757e25ea04e9320e6d
Otybrian <otieno.brian@student.moringaschool.com>
2022-02-20 04:11:47 +0300

    define method setUp that runs before the individual test credentials and also define method test_init that confirms whether a new instance of Credentials has been correctly initialized

--- credentials_test.py
+++ credentials_test.py
@@ -1,2 +1,26 @@
 import unittest
 from credentials import Credentials
+
+
+class TestCredentials(unittest.TestCase):
+    """
+    Test class that defines test cases for the class Credentials
+    """
+
+    def setUp(self):
+        """
+        This method runs before each individual test for credentials run
+        """
+
+        self.new_credential = Credentials('twitter', 'otbrayo', 'comE@5on')
+
+    def test_init(self):
+        """
+        A test case that checks whether a new instance of Credentials has been 
+        correctly initialized
+        """
+        self.assertEqual(self.new_credential.user_account, 'twitter')
+        self.assertEqual(self.new_credential.user_name, 'otbrayo')
+        self.assertEqual(self.new_credential.password, 'comE@5on')
+
+    
